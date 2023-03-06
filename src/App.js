3acaa7fc3d0388b02f9412685609210b88c83296
Jeff Tickle <jeff.tickle@protonmail.com>
2023-03-06 07:37:11 -0500

    Add a few more, this is fun

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Canvas2D from './Canvas2D.js'
 
 import './App.css'
 
+function nAbsSinFn (x, c) {
+  return -Math.abs(Math.sin(x * 2 * Math.PI)) + 1
+}
+
 function absCosFn (x, c) {
   return Math.abs(Math.cos(x * 2 * Math.PI))
 }
 
 function cosFn (x, c) {
   return Math.cos(x * 4 * Math.PI) / 2 + 0.5
+}
+
+function simpleQuadFn (x, c) {
+  return Math.pow((x * 2 - 1), 2)
 }
 
 function simpleFn (x, c) {
@@ -49,10 +57,14 @@ function App () {
     <div className="App">
       <h1>f(x) = x</h1>
       <Canvas2D draw={drawFactoryXOnly(simpleFn)} ratio={0.2}/>
-      <h1>f(x) = cos(x * 4pi) / 2 + 0.5</h1>
-      <Canvas2D draw={drawFactoryXOnly(cosFn)} ratio={0.2}/>
+      <h1>f(x) = (2x - 1)<sup>2</sup></h1>
+      <Canvas2D draw={drawFactoryXOnly(simpleQuadFn)} ratio={0.2}/>
       <h1>f(x) = abs(cos(x * 2pi))</h1>
       <Canvas2D draw={drawFactoryXOnly(absCosFn)} ratio={0.2}/>
+      <h1>f(x) = cos(x * 4pi) / 2 + 0.5</h1>
+      <Canvas2D draw={drawFactoryXOnly(cosFn)} ratio={0.2}/>
+      <h1>f(x) = -abs(sin(x * 2pi)) + 1</h1>
+      <Canvas2D draw={drawFactoryXOnly(nAbsSinFn)} ratio={0.2}/>
     </div>
   )
 }
